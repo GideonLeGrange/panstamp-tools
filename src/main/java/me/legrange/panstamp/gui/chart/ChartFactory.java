ec76dd399cdbbf8969191e5211f80776032aa2ce
Gideon le Grange <gideon@legrange.me>
2014-10-29 08:49:24 +0200

    Optimised ChartFactory a bit

--- src/main/java/me/legrange/panstamp/gui/chart/ChartFactory.java
+++ src/main/java/me/legrange/panstamp/gui/chart/ChartFactory.java
@@ -9,11 +9,11 @@ import me.legrange.panstamp.PanStamp;
 import me.legrange.panstamp.gui.model.DataModel;
 
 /**
- *
+ * Create and lookup charts in frames for different data sources. 
  * @author gideon
  */
 public class ChartFactory {
-    
+
     public static ChartFactory getFactory(DataModel model) {
         ChartFactory fact = facts.get(model);
         if (fact == null) {
@@ -22,16 +22,11 @@ public class ChartFactory {
         }
         return fact;
     }
-    
+
     public JFrame getSignalChart(PanStamp ps) {
         JFrame frame = signalCharts.get(ps);
         if (frame == null) {
-            frame = new JFrame();
-            Chart chart = new SignalChart(ps.getAddress(), model.getSignalDataSet(ps));
-            frame.setContentPane(chart);
-            frame.setTitle(chart.getTitle());
-            frame.setSize(chart.getSize());
-            frame.setResizable(false);
+            frame = frameChart(new SignalChart(ps.getAddress(), model.getSignalDataSet(ps)));
             signalCharts.put(ps, frame);
         }
         return frame;
@@ -40,22 +35,26 @@ public class ChartFactory {
     public JFrame getEndpointChart(Endpoint ep) {
         JFrame frame = endpointCharts.get(ep);
         if (frame == null) {
-            frame = new JFrame();
-            frame.setLayout(new BorderLayout());
-            Chart chart = new EndpointChart(model.getEndpointDataSet(ep));
-            frame.setTitle(chart.getTitle())    ;
-            frame.setContentPane(chart);
-            frame.setSize(chart.getSize());
-            frame.setResizable(false);
+            frame = frameChart(new EndpointChart(model.getEndpointDataSet(ep)));
             endpointCharts.put(ep, frame);
         }
+        return frame;
+    }
+
+    private JFrame frameChart(Chart chart) {
+        JFrame frame = new JFrame();
+        frame.setLayout(new BorderLayout());
+        frame.setTitle(chart.getTitle());
+        frame.setContentPane(chart);
+        frame.setSize(chart.getSize());
+        frame.setResizable(false);
         return frame;
     }
 
     private ChartFactory(DataModel model) {
         this.model = model;
     }
-    
+
     private final DataModel model;
     private final Map<PanStamp, JFrame> signalCharts = new HashMap<>();
     private final Map<Endpoint, JFrame> endpointCharts = new HashMap<>();
