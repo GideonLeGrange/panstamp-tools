4bb6ca45e88817330244fe4abe46cd1d5dffe110
Gideon le Grange <gideon@legrange.me>
2014-11-16 21:14:06 +0200

    Working on #4 - standard register display

--- src/main/java/me/legrange/panstamp/gui/model/tree/NetworkTreeNodeRenderer.java
+++ src/main/java/me/legrange/panstamp/gui/model/tree/NetworkTreeNodeRenderer.java
@@ -91,72 +91,84 @@ public class NetworkTreeNodeRenderer extends DefaultTreeCellRenderer {
     }
 
     private JPopupMenu getPanStampPopupMenu(final PanStampNode psn) {
-        JPopupMenu menu = new JPopupMenu(psn.toString());
-        final JMenuItem settingsItem = new JMenuItem("Settings...");
-        settingsItem.addActionListener(new ActionListener() {
+        JPopupMenu menu = popupMenus.get(psn);
+        if (menu == null) {
+            menu = new JPopupMenu(psn.toString());
+            final JMenuItem settingsItem = new JMenuItem("Settings...");
+            settingsItem.addActionListener(new ActionListener() {
 
-            @Override
-            public void actionPerformed(ActionEvent e) {    
-                new PanStampSettingsDialog(null, model, psn.getPanStamp()).setVisible(true);
-            }
-        });
-        menu.add(settingsItem);
-        final JMenuItem graphItem = new JMenuItem("RSSI/LQI Graph...");
-        graphItem.addActionListener(new ActionListener() {
-
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                ChartFactory.getFactory(model).getSignalChart(psn.getPanStamp()).setVisible(true);
-            }
-        });
-        menu.add(graphItem);
-        // register selection
-        menu.add(new JSeparator());
-        final JMenu regsMenu = new JMenu("Show Standard Registers");
-        final JRadioButtonMenuItem allItem = new JRadioButtonMenuItem("All");
-        final JRadioButtonMenuItem intItem = new JRadioButtonMenuItem("Interesting");
-        final JRadioButtonMenuItem noneItem = new JRadioButtonMenuItem("None", true);
-        ActionListener regL = new ActionListener() {
-
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                allItem.setSelected(false);
-                noneItem.setSelected(false);
-                intItem.setSelected(false);
-                if (e.getSource().equals(noneItem)) {
-                    noneItem.setSelected(true);
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    new PanStampSettingsDialog(null, model, psn.getPanStamp()).setVisible(true);
                 }
-                if (e.getSource().equals(allItem)) {
-                    allItem.setSelected(true);
-                }
-                if (e.getSource().equals(intItem)) {
-                    intItem.setSelected(true);
-                }
-            }
+            });
+            menu.add(settingsItem);
+            final JMenuItem graphItem = new JMenuItem("RSSI/LQI Graph...");
+            graphItem.addActionListener(new ActionListener() {
 
-        };
-        allItem.addActionListener(regL);
-        noneItem.addActionListener(regL);
-        intItem.addActionListener(regL);
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    ChartFactory.getFactory(model).getSignalChart(psn.getPanStamp()).setVisible(true);
+                }
+            });
+            menu.add(graphItem);
+            // register selection
+            menu.add(new JSeparator());
+            final JMenu regsMenu = new JMenu("Show Standard Registers");
+            final JRadioButtonMenuItem allItem = new JRadioButtonMenuItem("All");
+            final JRadioButtonMenuItem intItem = new JRadioButtonMenuItem("Interesting");
+            final JRadioButtonMenuItem noneItem = new JRadioButtonMenuItem("None", true);
+            ActionListener regL = new ActionListener() {
 
-        regsMenu.add(allItem);
-        regsMenu.add(intItem);
-        regsMenu.add(noneItem);
-        menu.add(regsMenu);
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    allItem.setSelected(false);
+                    noneItem.setSelected(false);
+                    intItem.setSelected(false);
+                    if (e.getSource().equals(noneItem)) {
+                        noneItem.setSelected(true);
+                        psn.setRegisterDisplay(PanStampNode.RegisterDisplay.NONE);
+                    }
+                    if (e.getSource().equals(allItem)) {
+                        allItem.setSelected(true);
+                        psn.setRegisterDisplay(PanStampNode.RegisterDisplay.ALL);
+                    }
+                    if (e.getSource().equals(intItem)) {
+                        intItem.setSelected(true);
+                        psn.setRegisterDisplay(PanStampNode.RegisterDisplay.INTERESTING);
+                    }
+                }
+
+            };
+            allItem.addActionListener(regL);
+            noneItem.addActionListener(regL);
+            intItem.addActionListener(regL);
+
+            regsMenu.add(allItem);
+            regsMenu.add(intItem);
+            regsMenu.add(noneItem);
+            menu.add(regsMenu);
+            popupMenus.put(psn, menu);
+        }
         return menu;
     }
 
     private JPopupMenu getEndpointPopupMenu(final EndpointNode epn) {
-        JPopupMenu menu = new JPopupMenu(epn.toString());
-        final JMenuItem graphItem = new JMenuItem("Data graph...");
-        graphItem.addActionListener(new ActionListener() {
+        JPopupMenu menu = popupMenus.get(epn);
+        if (menu == null) {
+            menu = new JPopupMenu(epn.toString());
+            final JMenuItem graphItem = new JMenuItem("Data graph...");
+            graphItem.addActionListener(new ActionListener() {
 
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                ChartFactory.getFactory(model).getEndpointChart(epn.getEndpoint()).setVisible(true);
-            }
-        });
-        menu.add(graphItem);
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    ChartFactory.getFactory(model).getEndpointChart(epn.getEndpoint()).setVisible(true);
+                }
+            });
+            menu.add(graphItem);
+            popupMenus.put(epn, menu);
+        }
+
         return menu;
     }
 
@@ -207,5 +219,6 @@ public class NetworkTreeNodeRenderer extends DefaultTreeCellRenderer {
     private static final String ICON_REGISTER = "register16x16.png";
     private static final String ICON_ENDPOINT = "endpoint16x16.png";
     private final Map<String, ImageIcon> icons = new HashMap<>();
+    private final Map<NetworkTreeNode, JPopupMenu> popupMenus = new HashMap<>();
     private final DataModel model;
 }
--- src/main/java/me/legrange/panstamp/gui/model/tree/PanStampNode.java
+++ src/main/java/me/legrange/panstamp/gui/model/tree/PanStampNode.java
@@ -4,6 +4,7 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javax.swing.tree.TreeNode;
 import me.legrange.panstamp.GatewayException;
 import me.legrange.panstamp.PanStamp;
 import me.legrange.panstamp.PanStampEvent;
@@ -15,6 +16,8 @@ import me.legrange.panstamp.Register;
  * @author gideon
  */
 class PanStampNode extends NetworkTreeNode implements PanStampListener {
+    
+    public enum RegisterDisplay { ALL, NONE, INTERESTING; };
 
     public PanStampNode(PanStamp ps) {
         super(ps);
@@ -28,6 +31,82 @@ class PanStampNode extends NetworkTreeNode implements PanStampListener {
     public String toString() {
         return String.format("Mote %d: %s", getPanStamp().getAddress(), getPanStamp().getName());
     }
+
+    @Override
+    public int getChildCount() {
+        switch (registerDisplay) {
+            case ALL : 
+                return super.getChildCount(); 
+            case NONE : 
+                int count = 0;
+                for (RegisterNode rn : nodes.values()) {
+                    if (rn.getRegister().getId() > 10) {
+                        count ++;
+                    }
+                }
+                return count;
+            case INTERESTING : 
+                count = 0;
+                for (RegisterNode rn : nodes.values()) {
+                    if ((rn.getRegister().getId() <= 10)) {
+                        if (rn.getRegister().hasValue()) {
+                            count ++;
+                        }
+                    }
+                    else {
+                        count ++;
+                    }
+                }
+                return count;
+        }
+        return 0;
+    }
+
+    @Override
+    public TreeNode getChildAt(int index) {
+        switch (registerDisplay) {
+            case ALL : 
+                return super.getChildAt(index);
+            case NONE : 
+                int idx = 0;
+                for (RegisterNode rn : nodes.values()) {
+                    if ((rn.getRegister().getId() > 10)) {
+                        if (idx == index) {
+                            return rn;
+                        }
+                        else {
+                            idx ++;
+                        }
+                    }
+                }
+                return null;
+            case INTERESTING :
+                idx = 0;
+                for (RegisterNode rn : nodes.values()) {
+                    if (rn.getRegister().getId() <= 10) {
+                        if (rn.getRegister().hasValue()) {
+                            if (idx == index) {
+                                return rn;
+                            }
+                            else {
+                                idx ++;
+                            }
+                        }
+                    }
+                    else {
+                            if (idx == index) {
+                                return rn;
+                            }
+                            else {
+                                idx ++;
+                            }
+                    }
+                }
+        }
+        return null;
+    }
+    
+    
 
     @Override
     protected void start() {
@@ -84,6 +163,13 @@ class PanStampNode extends NetworkTreeNode implements PanStampListener {
                 }
         }
     }
+    
+    void setRegisterDisplay(RegisterDisplay rd) {
+        if (rd != registerDisplay) {
+            registerDisplay = rd;
+            reload();
+        }
+    }
 
     private synchronized void addRegister(Register reg) {
         if (nodes.get(reg) == null) {
@@ -102,6 +188,6 @@ class PanStampNode extends NetworkTreeNode implements PanStampListener {
         nodes.put(reg, rn);
     }
 
-    private Map<Register, RegisterNode> nodes = new HashMap<>();
-
+    private final Map<Register, RegisterNode> nodes = new HashMap<>();
+    private RegisterDisplay registerDisplay = RegisterDisplay.NONE; 
 }
