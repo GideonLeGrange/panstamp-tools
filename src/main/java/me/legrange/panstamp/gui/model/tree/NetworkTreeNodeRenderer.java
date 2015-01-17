085896c88be6e505f72e4884a17d4c56ae8a14bf
Gideon le Grange <gideon@legrange.me>
2015-01-17 22:44:29 +0200

    Playing with JSON for persistent storage

--- src/main/java/me/legrange/panstamp/gui/model/tree/NetworkTreeNodeRenderer.java
+++ src/main/java/me/legrange/panstamp/gui/model/tree/NetworkTreeNodeRenderer.java
@@ -30,6 +30,10 @@ import me.legrange.panstamp.gui.PanStampSettingsDialog;
 import me.legrange.panstamp.gui.chart.ChartFactory;
 import me.legrange.panstamp.gui.model.DataModel;
 import me.legrange.panstamp.gui.model.Format;
+import me.legrange.panstamp.json.JsonException;
+import me.legrange.panstamp.json.JsonLoad;
+import me.legrange.panstamp.json.JsonStore;
+import me.legrange.swap.SWAPException;
 
 /**
  *
@@ -124,10 +128,22 @@ public class NetworkTreeNodeRenderer extends DefaultTreeCellRenderer {
 
                 @Override
                 public void actionPerformed(ActionEvent e) {
-                    /// put code here to select file/place to export to. 
+                    try {
+                        try {
+                            //System.out.println(JsonStore.networkAsJson(gn.getGateway()));
+                            new JsonLoad().loadJson(JsonStore.networkAsJson(gn.getGateway()));
+                            /// put code here to select file/place to export to. 
+                        } catch (JsonException ex) {
+                            Logger.getLogger(NetworkTreeNodeRenderer.class.getName()).log(Level.SEVERE, null, ex);
+                        }
+                    } catch (SWAPException ex) {
+                        Logger.getLogger(NetworkTreeNodeRenderer.class.getName()).log(Level.SEVERE, null, ex);
+                    } catch (GatewayException ex) {
+                        Logger.getLogger(NetworkTreeNodeRenderer.class.getName()).log(Level.SEVERE, null, ex);
+                    }
                 }
             });
-            
+            menu.add(exportItem);
             popupMenus.put(gn, menu);
         }
         return menu;
