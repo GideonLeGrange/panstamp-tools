87a6bd42b048e02b80bcfc9228925f7053cabcbc
Gideon le Grange <gideon@legrange.me>
2015-02-10 22:04:27 +0200

    Removed device menu from main menu for the moment

--- src/main/java/me/legrange/panstamp/gui/MainWindow.java
+++ src/main/java/me/legrange/panstamp/gui/MainWindow.java
@@ -134,7 +134,6 @@ public class MainWindow extends javax.swing.JFrame {
         quitItem = new javax.swing.JMenuItem();
         networkMenu = new javax.swing.JMenu();
         addNetworkItem = new javax.swing.JMenuItem();
-        deviceMenu = new javax.swing.JMenu();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setFont(new java.awt.Font("Courier", 0, 10)); // NOI18N
@@ -248,9 +247,6 @@ public class MainWindow extends javax.swing.JFrame {
 
         mainMenu.add(networkMenu);
 
-        deviceMenu.setText("Device");
-        mainMenu.add(deviceMenu);
-
         setJMenuBar(mainMenu);
 
         org.jdesktop.layout.GroupLayout layout = new org.jdesktop.layout.GroupLayout(getContentPane());
@@ -307,7 +303,6 @@ public class MainWindow extends javax.swing.JFrame {
     private javax.swing.JMenuItem addNetworkItem;
     private javax.swing.JPanel bottomPanel;
     private javax.swing.JMenuItem configMenuItem;
-    private javax.swing.JMenu deviceMenu;
     private javax.swing.JScrollPane eventPanel;
     private javax.swing.JTable eventTable;
     private javax.swing.JPanel leftPanel;
